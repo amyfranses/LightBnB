06971c96cf69028fa23a59e36dbade2ffe34b109
amyfranses <amyfranses@gmail.com>
2022-04-12 21:28:13 +0000

    add queries 1 folder

---
+++ 1_queries/5_all_my_reservations.sql
@@ -0,0 +1,16 @@
+SELECT reservations.id AS id, properties.title AS title, reservations.start_date AS start_date, properties.cost_per_night AS cost_per_night, AVG(property_reviews.rating) AS rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_dateLIMIT 10;
+
+
+-- SELECT reservations.id AS id, properties.title AS title, reservations.start_date AS start_date, properties.cost_per_night AS cost_per_night, AVG(property_reviews.rating) AS rating
+-- FROM reservations
+-- JOIN property_reviews ON reservations.id = property_reviews.reservation_id
+-- JOIN properties ON properties.id = owner_id
+-- JOIN users ON users.id = reservations.guest_id
+-- WHERE users.email =  'tristanjacobs@gmail.com'
+-- GROUP BY reservations.id, title, start_date, cost_per_night
