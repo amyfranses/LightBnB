5f38945aa935e1d0aba6a0f124c11c567a87934b
amyfranses <amyfranses@gmail.com>
2022-04-21 19:16:26 +0000

    remove duplicte README file from inner directory

--- 1_queries/5_all_my_reservations.sql
+++ 1_queries/5_all_my_reservations.sql
@@ -4,7 +4,8 @@ JOIN properties ON reservations.property_id = properties.id
 JOIN property_reviews ON properties.id = property_reviews.property_id
 WHERE reservations.guest_id = 1
 GROUP BY properties.id, reservations.id
-ORDER BY reservations.start_dateLIMIT 10;
+ORDER BY reservations.start_date
+LIMIT 10;
 
 
 -- SELECT reservations.id AS id, properties.title AS title, reservations.start_date AS start_date, properties.cost_per_night AS cost_per_night, AVG(property_reviews.rating) AS rating
